13db60405213f6a1d293c0516efcef84b012aea8
Lexx <joncanales2@gmail.com>
2020-10-23 09:15:57 -0400

    Updated db connection and required colors to help with better logging and added an error handler.

--- config/db.js
+++ config/db.js
@@ -4,9 +4,10 @@ const connectDB = async () => {
    const conn = await mongoose.connect(process.env.MONGO_URI, {
        useNewUrlParser: true,
        useCreateIndex: true,
-       useFindAndModify: false
+       useFindAndModify: false,
+       useUnifiedTopology: true
    })
-   console.log(`MongoDB connected ${conn.connection.host}`)
+   console.log(`MongoDB connected ${conn.connection.host}`.cyan.underline.bold)
 }
 
 module.exports = connectDB
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 // Morgan is a HTTP request middleware logger
+const colors = require("colors");
 const morgan = require("morgan");
 const connectDB = require("./config/db")
 
@@ -26,4 +27,12 @@ app.use("/api/v1/bootcamps", bootcamps)
 // Creating a port that will either access an environmental variable PORT or port 5000
 const PORT = process.env.PORT || 5000;
 // Console logging the environment that the port is running on and its port #
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
+
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) =>  {
+    console.log(`Error: ${err.message}`.red)
+    // Close server & exit process
+    server.close(() => process.exit(1))
+})
