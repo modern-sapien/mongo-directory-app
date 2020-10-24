03bb12ce8de346ac576325d0fa22f0a216483117
Lexx <joncanales2@gmail.com>
2020-10-24 12:18:52 -0400

    Full CRUD for courses.

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -81,7 +81,32 @@ exports.addCourse = asyncHandler(async (req, res, next) => {
 // @route   POST /api/v1/courses/:id
 // @access  Private
 exports.updateCourse = asyncHandler(async (req, res, next) => {
-  let course = await Course.findById(req.params.bootcampId);
+  let course = await Course.findById(req.params.id);
+
+  // if course ID not found
+  if (!course) {
+    return next(
+      new ErrorResponse(`No bootcamp with id of ${req.params.id}`),
+      404
+    );
+  }
+  
+  course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    data: course,
+  });
+});
+
+// @desc    Delete a course
+// @route   Delete /api/v1/courses/:id
+// @access  Private
+exports.deleteCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.findById(req.params.id);
 
   // if course ID not found
   if (!course) {
@@ -91,13 +116,10 @@ exports.updateCourse = asyncHandler(async (req, res, next) => {
     );
   }
 
-  course = await Course.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true,
-  });
+  await course.remove();
 
   res.status(200).json({
     success: true,
-    data: course,
+    data: {},
   });
 });
--- routes/courses.js
+++ routes/courses.js
@@ -3,14 +3,21 @@ const {
   getCourses,
   getCourse,
   addCourse,
-  updateCourse
+  updateCourse,
+  deleteCourse
 } = require("../controllers/courseController");
 const { update } = require("../models/Course");
 
 // mergeParams allows us to allow bootcamp.js router to send traffic
 const router = express.Router({ mergeParams: true });
 
-router.route("/").get(getCourses).post(addCourse);
-router.route("/:id").get(getCourse).put(updateCourse);
+router.route("/")
+    .get(getCourses)
+    .post(addCourse);
+
+router.route("/:id")
+    .get(getCourse)
+    .put(updateCourse)
+    .delete(deleteCourse);
 
 module.exports = router;
