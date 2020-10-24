331dec7c5a6d661887654daa8d6beefc1e4c09ce
Lexx <joncanales2@gmail.com>
2020-10-24 16:59:08 -0400

    receiving cookie with token inside of it.

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const dotenv = require("dotenv");
 const morgan = require("morgan");
 const colors = require("colors");
 const fileupload = require("express-fileupload");
+const cookieParser = require("cookie-parser");
 const errorHandler = require("./middleware/error");
 const connectDB = require("./config/db");
 
@@ -15,6 +16,9 @@ const app = express();
 
 // Body Parser
 app.use(express.json());
+
+// Cookie parser
+app.use(cookieParser());
 
 // Connect to database
 connectDB();
