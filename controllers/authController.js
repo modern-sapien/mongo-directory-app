8dac64fdd4411e43c19daa1d3f058a9d1f6f1b88
Lexx <joncanales2@gmail.com>
2020-10-24 17:26:42 -0400

    Updating user auth for cookie expiration and sending secure flag on cookie while in production.

--- controllers/authController.js
+++ controllers/authController.js
@@ -59,8 +59,13 @@ const sendTokenResponse = (user, statusCode, res) => {
     const token = user.getSignedJwtToken();
 
     const options = {
+        // sets and expiration 30 days from time of cookie creation
         expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
         httpOnly: true
+    }
+    // When we are in production secure flag is on cookie.
+    if (process.env.NODE_ENV === "production" ) {
+        options.secure = true;
     }
 
     res
