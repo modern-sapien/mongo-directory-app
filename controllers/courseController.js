b2f79be145f679ef44996b7c4d853345a6ef3ce8
Lexx <joncanales2@gmail.com>
2020-10-24 12:12:18 -0400

    Adding course by bootcamp id finished, working on updating course & routing appropriately.

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,6 +1,7 @@
 const ErrorResponse = require("../utils/errorResponse");
 const asyncHandler = require("../middleware/async");
 const Course = require("../models/Course");
+const Bootcamp = require("../models/Bootcamp");
 
 // @desc    Get all courses
 // @route   GET /api/v1/courses
@@ -54,19 +55,46 @@ exports.getCourse = asyncHandler(async (req, res, next) => {
 // @desc    Add a course
 // @route   POST /api/v1/bootcamps/:bootcampId/courses
 // @access  Private
-exports.getCourse = asyncHandler(async (req, res, next) => {
-  const course = await Course.findById(req.params.id).populate({
-    path: "bootcamp",
-    select: "name description",
+exports.addCourse = asyncHandler(async (req, res, next) => {
+  // manually assigned bootcamp in course model to the param ID provided by a user
+  req.body.bootcamp = req.params.bootcampId;
+  // searches for bootcamp to see if we can add it to the "bootcamp" field in course model
+  const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+
+  // if course ID not found
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`No bootcamp with id of ${req.params.bootcampId}`),
+      404
+    );
+  }
+
+  const course = await Course.create(req.body);
+
+  res.status(200).json({
+    success: true,
+    data: course,
   });
+});
+
+// @desc    Update a course
+// @route   POST /api/v1/courses/:id
+// @access  Private
+exports.updateCourse = asyncHandler(async (req, res, next) => {
+  let course = await Course.findById(req.params.bootcampId);
 
   // if course ID not found
   if (!course) {
     return next(
-      new ErrorResponse(`No course with id of ${req.params.id}`),
+      new ErrorResponse(`No bootcamp with id of ${req.params.id}`),
       404
     );
   }
+
+  course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
 
   res.status(200).json({
     success: true,
--- routes/courses.js
+++ routes/courses.js
@@ -1,10 +1,16 @@
 const express = require("express");
-const { getCourses, getCourse } = require("../controllers/courseController");
+const {
+  getCourses,
+  getCourse,
+  addCourse,
+  updateCourse
+} = require("../controllers/courseController");
+const { update } = require("../models/Course");
 
 // mergeParams allows us to allow bootcamp.js router to send traffic
-const router = express.Router({ mergeParams: true});
+const router = express.Router({ mergeParams: true });
 
-router.route("/").get(getCourses);
-router.route("/:id").get(getCourse);
+router.route("/").get(getCourses).post(addCourse);
+router.route("/:id").get(getCourse).put(updateCourse);
 
 module.exports = router;
